e5b0e0a1d65926d1903c38263aae05188940d9fc
Darya Pchelintseva <raelin@mail.ru>
2024-01-15 23:10:30 +0300

    feat: do second task

---
+++ 02-write-file/index.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+const process = require('process');
+
+fs.open('./02-write-file/text.txt', 'w', (err) => err && console.error(err));
+const writeStream = fs.createWriteStream('./02-write-file/text.txt');
+console.log('Напишите что-нибудь');
+
+const readline = require('readline');
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+rl.on('line', (input) => {
+  writeStream.write(input + '\n');
+
+  if (input.toLocaleLowerCase() === 'exit') {
+    console.log('Текст записан.');
+    writeStream.end();
+    process.exit();
+  }
+});
+process.on('exit', () => {
+  console.log('Пока!');
+});
