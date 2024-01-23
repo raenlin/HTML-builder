b31c34ccc768742562908ca84eceda9061f90805
Darya Pchelintseva <raelin@mail.ru>
2024-01-23 19:34:40 +0300

    fix: fix mistakes

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,30 +1,39 @@
 const fs = require('fs');
-const path = require('node:path');
+const path = require('path');
 
-const copyFiles = () => {
-  const sourceDirectory = path.join(__dirname, 'files');
-  const newDirectory = path.join(__dirname, 'files-copy');
-  fs.mkdir(newDirectory, { recursive: true }, (err) => {
-    if (err) {
-      console.log(err);
-    }
-    fs.readdir(sourceDirectory, (err, files) => {
+const sourceDirectory = path.join(__dirname, 'files');
+const newDirectory = path.join(__dirname, 'files-copy');
+fs.mkdir(newDirectory, { recursive: true }, (err) => {
+  if (err) {
+    console.log(err);
+  }
+});
+fs.readdir(newDirectory, (err, files) => {
+  if (err) {
+    console.log(err);
+  }
+  for (const file of files) {
+    fs.unlink(path.join(newDirectory, file), (err) => {
       if (err) {
         console.log(err);
-        return;
       }
-      files.forEach((file) => {
-        const sourceFiles = path.join(sourceDirectory, file);
-        const newFiles = path.join(newDirectory, file);
-        fs.copyFile(sourceFiles, newFiles, (err) => {
-          if (err) {
-            console.log(err);
-            return;
-          }
-        });
+    });
+  }
+});
+fs.readdir(sourceDirectory, (err, files) => {
+  if (err) {
+    console.log(err);
+  }
+  for (const file of files) {
+    fs.readFile(path.join(sourceDirectory, file), (err, data) => {
+      if (err) {
+        console.log(err);
+      }
+      fs.writeFile(path.join(newDirectory, file), data, (err) => {
+        if (err) {
+          console.log(err);
+        }
       });
     });
-  });
-};
-
-copyFiles();
+  }
+});
